868a45ff3c26cc0193651bf23f757f3275b46e4e
bep20 <sadlovestory7358@gmail.com>
2025-01-18 05:53:20 +0200

    Update 7.769068e3.js

--- assets/js/7.769068e3.js
+++ assets/js/7.769068e3.js
@@ -1,1 +1,119 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{368:function(t,e,r){"use strict";r.r(e);var o=r(44),a=Object(o.a)({},(function(){var t=this,e=t.$createElement,r=t._self._c||e;return r("ContentSlotsDistributor",{attrs:{"slot-key":t.$parent.slotKey}},[r("hr"),t._v(" "),r("p",[t._v("Create your own ERC20 or BEP20 Token in less than a minute with the most used Smart Contract Generator for ERC20 and BEP20 Token.")]),t._v(" "),r("p",[t._v("Token Generator is a distributed application that runs on the Blockchain (Ethereum or BSC network), using specially-developed Smart Contracts to enable users to build their ERC20 or BEP20 Tokens.")]),t._v(" "),r("p",[t._v("Easily deploy Smart Contract for a Standard, Capped, Mintable, Burnable, Payable|Operable Token.")]),t._v(" "),r("p",[r("strong",[t._v("No login. No setup. No coding required.")])]),t._v(" "),r("h2",{attrs:{id:"erc20"}},[r("a",{staticClass:"header-anchor",attrs:{href:"#erc20"}},[t._v("#")]),t._v(" ERC20")]),t._v(" "),r("ul",[r("li",[r("a",{attrs:{href:"https://bep20.github.io/erc20-generator/",target:"_blank",rel:"noopener noreferrer"}},[t._v("Create ERC20"),r("OutboundLink")],1)]),t._v(" "),r("li",[r("RouterLink",{attrs:{to:"/docs/how-to-create-erc20-token/"}},[t._v("Documentation")])],1)]),t._v(" "),r("h2",{attrs:{id:"bep20"}},[r("a",{staticClass:"header-anchor",attrs:{href:"#bep20"}},[t._v("#")]),t._v(" BEP20")]),t._v(" "),r("ul",[r("li",[r("a",{attrs:{href:"https://bep20.github.io/bep20-generator/",target:"_blank",rel:"noopener noreferrer"}},[t._v("Create BEP20"),r("OutboundLink")],1)]),t._v(" "),r("li",[r("RouterLink",{attrs:{to:"/docs/how-to-create-bep20-token/"}},[t._v("Documentation")])],1)])])}),[],!1,null,null,null);e.default=a.exports}}]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [7],
+  {
+    368: function (t, e, r) {
+      "use strict";
+      r.r(e);
+      var o = r(44),
+        a = Object(o.a)(
+          {},
+          function () {
+            var t = this,
+              e = t.$createElement,
+              r = t._self._c || e;
+            return r("ContentSlotsDistributor", { attrs: { "slot-key": t.$parent.slotKey } }, [
+              r("hr"),
+              t._v(" "),
+              r("p", [
+                t._v(
+                  "Create your own ERC20, BEP20, Solana, or Polygon Token in less than a minute with the most used Smart Contract Generator."
+                ),
+              ]),
+              t._v(" "),
+              r("p", [
+                t._v(
+                  "Token Generator is a distributed application that runs on the Blockchain (Ethereum, BSC, Solana, or Polygon network), using specially-developed Smart Contracts to enable users to build their Tokens."
+                ),
+              ]),
+              t._v(" "),
+              r("p", [
+                t._v(
+                  "Easily deploy Smart Contract for a Standard, Capped, Mintable, Burnable, Payable|Operable Token."
+                ),
+              ]),
+              t._v(" "),
+              r("p", [r("strong", [t._v("No login. No setup. No coding required.")])]),
+              t._v(" "),
+              r("h2", { attrs: { id: "erc20" } }, [
+                r("a", { staticClass: "header-anchor", attrs: { href: "#erc20" } }, [t._v("#")]),
+                t._v(" ERC20"),
+              ]),
+              t._v(" "),
+              r("ul", [
+                r("li", [
+                  r(
+                    "a",
+                    { attrs: { href: "https://bep20.github.io/erc20-generator/", target: "_blank", rel: "noopener noreferrer" } },
+                    [t._v("Create ERC20"), r("OutboundLink")],
+                    1
+                  ),
+                ]),
+                t._v(" "),
+                r("li", [r("RouterLink", { attrs: { to: "/docs/how-to-create-erc20-token/" } }, [t._v("Documentation")])], 1),
+              ]),
+              t._v(" "),
+              r("h2", { attrs: { id: "bep20" } }, [
+                r("a", { staticClass: "header-anchor", attrs: { href: "#bep20" } }, [t._v("#")]),
+                t._v(" BEP20"),
+              ]),
+              t._v(" "),
+              r("ul", [
+                r("li", [
+                  r(
+                    "a",
+                    { attrs: { href: "https://bep20.github.io/bep20-generator/", target: "_blank", rel: "noopener noreferrer" } },
+                    [t._v("Create BEP20"), r("OutboundLink")],
+                    1
+                  ),
+                ]),
+                t._v(" "),
+                r("li", [r("RouterLink", { attrs: { to: "/docs/how-to-create-bep20-token/" } }, [t._v("Documentation")])], 1),
+              ]),
+              t._v(" "),
+              r("h2", { attrs: { id: "solana" } }, [
+                r("a", { staticClass: "header-anchor", attrs: { href: "#solana" } }, [t._v("#")]),
+                t._v(" Solana"),
+              ]),
+              t._v(" "),
+              r("ul", [
+                r("li", [
+                  r(
+                    "a",
+                    { attrs: { href: "https://bep20.github.io/solana-generator/", target: "_blank", rel: "noopener noreferrer" } },
+                    [t._v("Create Solana Token"), r("OutboundLink")],
+                    1
+                  ),
+                ]),
+                t._v(" "),
+                r("li", [r("RouterLink", { attrs: { to: "/docs/how-to-create-solana-token/" } }, [t._v("Documentation")])], 1),
+              ]),
+              t._v(" "),
+              r("h2", { attrs: { id: "polygon" } }, [
+                r("a", { staticClass: "header-anchor", attrs: { href: "#polygon" } }, [t._v("#")]),
+                t._v(" Polygon"),
+              ]),
+              t._v(" "),
+              r("ul", [
+                r("li", [
+                  r(
+                    "a",
+                    { attrs: { href: "https://bep20.github.io/polygon-generator/", target: "_blank", rel: "noopener noreferrer" } },
+                    [t._v("Create Polygon Token"), r("OutboundLink")],
+                    1
+                  ),
+                ]),
+                t._v(" "),
+                r("li", [r("RouterLink", { attrs: { to: "/docs/how-to-create-polygon-token/" } }, [t._v("Documentation")])], 1),
+              ]),
+            ]);
+          },
+          [],
+          !1,
+          null,
+          null,
+          null
+        );
+      e.default = a.exports;
+    },
+  },
+]);
